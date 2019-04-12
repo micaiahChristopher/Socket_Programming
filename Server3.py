70b7d11a0b4a22dbe206af95b86c9660c50812b9
micaiahChristopher <christopher.micaiah@gmail.com>
2019-04-12 00:15:23 -0700

    Commit Server3

--- Server3.py
+++ Server3.py
@@ -10,13 +10,14 @@ s.bind((host, port))
 
 s.listen(5000)
 
-message = "We connected and "
+message = "Hello starshine, the earth says hello"
 
 while 1:
     conn, addr = s.accept()
     print("Connected with", addr[0], "+ ", str(addr[1]))
 
     data = conn.recv(2048)
+    print(data.decode())
     reply = "Okay" + data.decode()
     if not data:
         break
